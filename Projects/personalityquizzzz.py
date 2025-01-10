b5bbc6e03e46f5f2ae6282724dee98517da34b04
Spencer2029 <spencerforman@seattleacademy.org>
2025-01-10 17:00:05 +0000

    work

--- Projects/personalityquizzzz.py
+++ Projects/personalityquizzzz.py
@@ -1,66 +1,91 @@
-# begining of quiz
+# beginning
+# g of quiz
 
-nice_points = 0
-naughty_points = 0
+good_points = 0
+bad_points = 0
 
-# middle of quiz
+# middle
 
-answer = input(" are you nice A) nice, or B) naughty?\n")
+answer = input(" are you good A) good, or B) bad?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
 answer = input(" do you like A) dogs, or B) cats?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
 
 
 
 answer = input(" do you wear socks with sandals A) no, or B) yes?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
 answer = input(" do you like the color dark red A) yes, or B) no?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
 answer = input(" do you like dark green like forest green A) yes, or B) no?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
 answer = input(" do you like peppermint A) yes, or B) no?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
 answer = input(" do you kick puppies A) no, or B) yes?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
+
+answer = input(" do you know that Honey is bee barf A) yes, or B) no?\n")
+if answer == "A":
+    good_points += 1
+elif answer == "B":
+    bad_points += 1
 
     answer = input(" do you find yourself acting like an animal? A) no what ew?, or B) yes all the time!\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
+
+answer = input(" do you find yourself wearing fluffy tails and cat ears A) no, or B) yesB?\n")
+if answer == "A":
+    good_points += 1
+elif answer == "B":
+    bad_points += 1
 
 answer = input(" do you like cookies A) yup, or B) no?\n")
 if answer == "A":
-    nice_points += 1
+    good_points += 1
 elif answer == "B":
-    naughty_points += 1
+    bad_points += 1
 
-# end of quiz
+answer = input(" do you prefer to go without shoes? A) yup, or B) no?\n")
+if answer == "A":
+    good_points += 1
+elif answer == "B":
+    bad_points += 1
 
-if nice_points > naughty_points:
-    print("you are nice")
-elif naughty_points > nice_points:
-    print("you are naughty")
-elif nice_points == naughty_points:
+answer = input(" are you alergic to peanut butter A) no, or B) yes?\n")
+if answer == "A":
+    good_points += 1
+elif answer == "B":
+    bad_points += 100
+
+# end
+
+if good_points > bad_points:
+    print("you are good")
+elif bad_points > good_points:
+    print("you are bad")
+elif good_points == bad_points:
     print("i cant tell... go away")

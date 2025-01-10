4523feea833a17df684565a9af662bdfb0860830
Spencer2029 <spencerforman@seattleacademy.org>
2025-01-10 16:42:06 +0000

    adding #

--- Projects/personalityquizzzz.py
+++ Projects/personalityquizzzz.py
@@ -1,5 +1,10 @@
+# begining of quiz
+
 nice_points = 0
-naughty_points = 0 
+naughty_points = 0
+
+# middle of quiz
+
 answer = input(" are you nice A) nice, or B) naughty?\n")
 if answer == "A":
     nice_points += 1
@@ -38,6 +43,21 @@ if answer == "A":
     nice_points += 1
 elif answer == "B":
     naughty_points += 1
+
+    answer = input(" do you find yourself acting like an animal? A) no what ew?, or B) yes all the time!\n")
+if answer == "A":
+    nice_points += 1
+elif answer == "B":
+    naughty_points += 1
+
+answer = input(" do you like cookies A) yup, or B) no?\n")
+if answer == "A":
+    nice_points += 1
+elif answer == "B":
+    naughty_points += 1
+
+# end of quiz
+
 if nice_points > naughty_points:
     print("you are nice")
 elif naughty_points > nice_points:

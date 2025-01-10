aeb5616db3a530dfb21c7fee3fbc0185a50476a5
Spencer2029 <spencerforman@seattleacademy.org>
2025-01-10 20:15:59 +0000

    lines

---
+++ Projects/code_art.py
@@ -0,0 +1,91 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.speed(0)
+
+t.color("hot pink")
+
+for i in range (250) :
+    t.forward(3 + i)
+    t.left(72 + 1) 
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.color("light pink")
+
+for i in range (300) :
+    t.forward(7 + i)
+    t.left(72 + 1) 
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.color("Crimson")
+
+for i in range (250) :
+    t.forward(4 + i)
+    t.left(72 + 1)
+
+t.penup()
+t.goto(0,0)
+t.pendown() 
+
+t.color("pink")
+
+for i in range (250) :
+    t.forward(20 + i)
+    t.left(72 + 1) 
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.color("salmon")
+
+for i in range (250) :
+    t.forward(9 + i)
+    t.left(72 + 1) 
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.color("hotpink")
+
+for i in range (250) :
+    t.forward(10 + i)
+    t.left(72 + 1) 
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.color("red")
+
+for i in range (250) :
+    t.forward(4 + i)
+    t.left(45 + 1) 
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.color("light pink")
+
+for i in range (250) :
+    t.forward(3 + i)
+    t.left(45 + 1) 
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+t.penup()
+t.goto(0,0)
+t.pendown()
+
+turtle.exitonclick()

7b0f0306a1afd0ee3c2226f4db07301ae58865a0
Spencer2029 <spencerforman@seattleacademy.org>
2025-02-13 18:26:16 +0000

    asjfkskjnw

--- Projects/project4.py
+++ Projects/project4.py
@@ -4,7 +4,7 @@ import codesters, random
 from codesters import StageClass
 stage = StageClass()
 stage.disable_floor()
-player = codesters.Sprite("Lolaaa2")
+player = codesters.Sprite("Lolaaa2",0,-180)
 player.set_size(0.25)
 stage.set_background("doghouse3")
 object_speed = -5
@@ -16,7 +16,37 @@ def falling_objects():
     global object_speed, bones
     if bones < 10:
         x = random.randint ( -250, 250 ) 
-        object = codesters.Sprite("bone",x,250)
+        object = codesters.Sprite("bone",0,600)
         object.set_size(.25)
+        object.go_to(x,250)
         object.set_y_speed(object_speed)
 stage.event_interval(falling_objects, 2)
+
+# Section 3 - collision 
+
+def collision(player, object):
+    global bones 
+
+    if object.get_image_name() == "bone":
+        stage.remove_sprite(object)
+        bones += 1
+        if bones == 0:
+            player.say("you have 0 bones", 5)
+        else:
+            player.say(f"{bones}",0.5)
+
+player.event_collision(collision)
+
+# Section 4 - controlls 
+
+# Right Key
+def go_right():
+    player.move_right(10)
+
+player.event_key("right", go_right)
+
+# Left Key
+def go_left():
+    player.move_left(10)
+
+player.event_key("left", go_left)
